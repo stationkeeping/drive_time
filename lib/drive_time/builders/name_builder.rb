a90b69f0d503ee0cf7e07b92fb0cdc85bc55be5c
Pedr Browne <pedr.browne@gmail.com>
2013-07-04 18:17:19 +0100

    Removed test exception

--- lib/drive_time/builders/name_builder.rb
+++ lib/drive_time/builders/name_builder.rb
@@ -2,10 +2,11 @@ module DriveTime
 
   # Take a series of name fields. Look up their values and assemble them into a single id
   class NameBuilder < JoinBuilder
-      
+
     protected
 
       def process_value(value)
+
         # Add a period to initials
         if value.length == 1
           return "#{value}."
